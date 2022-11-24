7ca49051c4e7abba2b92824743b71aad57645c37
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-24 14:59:30 +0000

    can't open file 'manage.py': [Errno 2]

--- book_sessions/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path, include
-from book_sessions import views
-
-urlpatterns = [
-    path('booking/', views.booking.as_view(), name="booking"),
-    path('booking/', include('booking.urls')),
-]
